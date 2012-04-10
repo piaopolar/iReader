f14d671b4abb7f5080e2b01619ca051e1055e777
piao <piao.polar@gmail.com>
2012-04-10 20:46:13 +0800

    add some useful str function

--- BaseCode/BaseFunc.h
+++ BaseCode/BaseFunc.h
@@ -16,5 +16,13 @@ std::string MyTrim(char sz[]);
 std::string MyTrim(std::string &str);
 std::string UTF8ToAnsi(const std::string &strIn, std::string &strOut);
 std::string AnsiToUTF8(const std::string &strIn, std::string &strOut);
+
+std::string ANSI_2_UTF8(const std::string &strANSI);
+std::wstring ANSI_2_UTF16(const std::string &strANSI);
+std::string UTF8_2_ANSI(const std::string &strUTF8);
+std::wstring UTF8_2_UTF16(const std::string &strUTF8);
+std::string UTF16_2_ANSI(const std::wstring &strUTF16);
+std::string UTF16_2_UTF8(const std::wstring &strUTF16);
+
 void ReplaceStdString(std::string &str, const std::string &strSrc,
 					  const std::string &strDest);
